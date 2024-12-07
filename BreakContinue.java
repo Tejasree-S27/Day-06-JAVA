6de870fd867e3317cc0cfc219858d6646d3d2f9e
Tejasree-S27 <stejasree348@gmail.com>
2024-12-07 11:00:21 +0530

    Add files via upload

---
+++ BreakContinue.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class BreakContinue 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Break example");
+        for(int i=0;i<n;i++)
+        {
+            if(i==2)
+            {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("Continue example");
+        for(int i=0;i<n;i++)
+        {
+            if(i==2)
+            {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
