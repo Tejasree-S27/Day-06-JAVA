9c33a2c7d60f89eb15a16bd0be5eef8313c2611c
Tejasree-S27 <stejasree348@gmail.com>
2024-12-07 12:09:09 +0530

    Add files via upload

---
+++ Array1D.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Array1D
+{
+    public static void main(String args[])
+{
+//Initialization of array
+int arr[]={22,44,565,789,200};
+//Accessing of array element
+System.out.println(arr[2]);
+System.out.println(arr[4]);
+//Modification of array
+arr[2]=465;
+arr[4]=200;
+System.out.println(Arrays.toString(arr));
+}
+}
